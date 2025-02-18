76bb8f78db4a3661eb37cf70a0b3edd741d5333e
Abhishek Malvadkar <abhi@dev.com>
2025-02-18 11:57:39 +0530

    Initial commit

---
+++ src/main/java/com/amalvadkar/scia/SpringConditionalInActionApplication.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.scia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConditionalInActionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConditionalInActionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amalvadkar/scia/SpringConditionalInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amalvadkar.scia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringConditionalInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
